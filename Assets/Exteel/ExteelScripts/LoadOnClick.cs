0e51b30277f73375b30ab8d8f43c7b1d253e2d81
Andy Tu <andysctu@gmail.com>
2016-02-14 02:04:47 -0500

    login working

--- Assets/Exteel/ExteelScripts/LoadOnClick.cs
+++ Assets/Exteel/ExteelScripts/LoadOnClick.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LoadOnClick : MonoBehaviour {
 
-	public void LoadScene(int scene){
-		Application.LoadLevel (scene);
+	public string LoginURL = "https://afternoon-temple-1885.herokuapp.com/login";
+	public InputField user, pass;
+	public Text error;
+
+	public void Login(){
+		WWWForm form = new WWWForm();
+		form.AddField("username", user.text);
+		form.AddField("password", pass.text);
+
+		WWW www = new WWW(LoginURL, form);
+
+		while (!www.isDone) {}
+	
+		if (www.responseHeaders["STATUS"] == "HTTP/1.1 200 OK") {
+			Application.LoadLevel (1);
+		}
+		else {
+			error.gameObject.SetActive(true);
+		}
 	}
 }
---
+++ Assets/Exteel/ExteelScripts/Lobby.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lobby : MonoBehaviour {
+
+	public GameObject Mech;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("H");
+		Mech.SetActive(true);
+	}
+
+	void Awake() {
+		Debug.Log("Hi");
+		Mech.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
