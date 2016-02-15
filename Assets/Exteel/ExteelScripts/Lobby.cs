7f69552c463004a822182f618f3313c4f2c061ad
Andy Tu <andysctu@gmail.com>
2016-02-15 18:22:59 -0500

    Hangar scene

--- Assets/Exteel/ExteelScripts/Lobby.cs
+++ Assets/Exteel/ExteelScripts/Lobby.cs
@@ -7,17 +7,20 @@ public class Lobby : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("H");
 		Mech.SetActive(true);
 	}
 
-	void Awake() {
-		Debug.Log("Hi");
-		Mech.SetActive(true);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnGUI(){
+		Debug.Log("Drawing");
+		GUI.BeginGroup(new Rect(Screen.width/2-150,Screen.height/2-150,300,300));
+		GUI.Box(new Rect(0,0,300,300),"This is the title of a box");
+		GUI.Button(new Rect(0,25,100,20),"I am a button");
+		GUI.Label (new Rect (0, 50, 100, 20), "I'm a Label!");
+//		toggleTxt = GUI.Toggle(Rect(0, 75, 200, 30), toggleTxt, "I am a Toggle button");
+//		toolbarInt = GUI.Toolbar (Rect (0, 110, 250, 25), toolbarInt, toolbarStrings);
+//		selGridInt = GUI.SelectionGrid (Rect (0, 160, 200, 40), selGridInt, selStrings, 2);
+//		hSliderValue = GUI.HorizontalSlider (Rect (0, 210, 100, 30), hSliderValue, 0.0, 1.0);
+//		hSbarValue = GUI.HorizontalScrollbar (Rect (0, 230, 100, 30), hSbarValue, 1.0, 0.0, 10.0);
+		GUI.EndGroup ();
 	}
 }
--- Assets/Exteel/ExteelScripts/NetworkManagerCustom.cs
+++ Assets/Exteel/ExteelScripts/NetworkManagerCustom.cs
@@ -17,49 +17,49 @@ public class NetworkManagerCustom : NetworkManager {
 
 	}
 
-	private bool first = true;
-	public void StartupHost(){
-		SetPort ();
-		NetworkManager.singleton.StartHost ();
-	}
-
-	public void JoinGame(){
-		SetIPAddress ();
-		SetPort ();
-		NetworkManager.singleton.StartClient ();
-	}
-
-	void SetPort(){
-		NetworkManager.singleton.networkPort = 7777;
-	}
-
-	void SetIPAddress(){
-		string ipAddress = GameObject.Find ("InputFieldIPAddress").transform.FindChild ("Text").GetComponent<Text> ().text;
-		NetworkManager.singleton.networkAddress = ipAddress;
-	}
-
-	void OnLevelWasLoaded(int level){
-		if (level == 1 && !first)  { // Home
-//			Debug.Log ("lvl 1");
-			StartCoroutine(SetupMenuSceneButtons());
-		} else if (level == 3) { // Game
-//			Debug.Log ("lvl 2");
-			SetupOtherSceneButtons ();
-		}
-		first = false;
-	}
-
-	IEnumerator SetupMenuSceneButtons(){
-		yield return new WaitForSeconds (0.2f);
-		GameObject.Find ("ButtonStartHost").GetComponent<Button> ().onClick.RemoveAllListeners ();
-		GameObject.Find ("ButtonStartHost").GetComponent<Button> ().onClick.AddListener(StartupHost);
-
-		GameObject.Find ("ButtonJoinGame").GetComponent<Button> ().onClick.RemoveAllListeners ();
-		GameObject.Find ("ButtonJoinGame").GetComponent<Button> ().onClick.AddListener(JoinGame);
-	}
-
-	void SetupOtherSceneButtons(){
-		GameObject.Find ("ButtonDisconnect").GetComponent<Button> ().onClick.RemoveAllListeners ();
-		GameObject.Find ("ButtonDisconnect").GetComponent<Button> ().onClick.AddListener(NetworkManager.singleton.StopHost);
-	}
+//	private bool first = true;
+//	public void StartupHost(){
+//		SetPort ();
+//		NetworkManager.singleton.StartHost ();
+//	}
+//
+//	public void JoinGame(){
+//		SetIPAddress ();
+//		SetPort ();
+//		NetworkManager.singleton.StartClient ();
+//	}
+//
+//	void SetPort(){
+//		NetworkManager.singleton.networkPort = 7777;
+//	}
+//
+//	void SetIPAddress(){
+//		string ipAddress = GameObject.Find ("InputFieldIPAddress").transform.FindChild ("Text").GetComponent<Text> ().text;
+//		NetworkManager.singleton.networkAddress = ipAddress;
+//	}
+//
+//	void OnLevelWasLoaded(int level){
+//		if (level == 1 && !first)  { // Home
+////			Debug.Log ("lvl 1");
+//			StartCoroutine(SetupMenuSceneButtons());
+//		} else if (level == 3) { // Game
+////			Debug.Log ("lvl 2");
+//			SetupOtherSceneButtons ();
+//		}
+//		first = false;
+//	}
+//
+//	IEnumerator SetupMenuSceneButtons(){
+//		yield return new WaitForSeconds (0.2f);
+//		GameObject.Find ("ButtonStartHost").GetComponent<Button> ().onClick.RemoveAllListeners ();
+//		GameObject.Find ("ButtonStartHost").GetComponent<Button> ().onClick.AddListener(StartupHost);
+//
+//		GameObject.Find ("ButtonJoinGame").GetComponent<Button> ().onClick.RemoveAllListeners ();
+//		GameObject.Find ("ButtonJoinGame").GetComponent<Button> ().onClick.AddListener(JoinGame);
+//	}
+//
+//	void SetupOtherSceneButtons(){
+//		GameObject.Find ("ButtonDisconnect").GetComponent<Button> ().onClick.RemoveAllListeners ();
+//		GameObject.Find ("ButtonDisconnect").GetComponent<Button> ().onClick.AddListener(NetworkManager.singleton.StopHost);
+//	}
 }
