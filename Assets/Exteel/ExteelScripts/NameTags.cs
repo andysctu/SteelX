46d7e7ea1f4cc6707e5bacc50f82f2b761146d80
Bobby <tu.sh.bobby@gmail.com>
2016-06-08 19:46:15 -0400

    NameTagsV1Unfinished

---
+++ Assets/Exteel/ExteelScripts/NameTags.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class NameTags : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        List<GameObject> PlayerNames = gm.playerInfo.Keys.ToList();
+        List<GameObject> aList = new List<GameObject> ();
+	    for (int i = 0; i < PlayerNames.Count; i++)
+        {
+            aList.Add(GetComponent<GUIText>().text = PlayerNames[i].name);
+        }
+        return aList;
+        Debug.Log(PlayerNames);
+	}
+ 
+}
+
