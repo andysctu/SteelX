f2bcc2b184ab91d158fc6094afe3834ae4b8f783
Andy Tu <andy.tu@originate.com>
2016-06-08 17:58:18 -0700

    Added helpful comments

--- Assets/Exteel/ExteelScripts/NameTags.cs
+++ Assets/Exteel/ExteelScripts/NameTags.cs
@@ -10,19 +10,26 @@ public class NameTags : MonoBehaviour {
 	Camera cam;
 
 	void Start () 
-    {
+    {	
+		// Try to find the drone
 		drone = GameObject.Find ("Drone");
 		if (drone == null) {
+			// if we can't find it, it probably did not load yet, we will find it later
 			Debug.Log ("Drone is null");
 		}
 		cam = GameObject.Find("Camera").GetComponent<Camera>();
 	}
 
+	// OnGUI runs every frame, like Update, but just for GUI stuff like labels and scoreboard, etc
 	void OnGUI() {
+		// If we didn't find the drone yet, try to find it
 		if (drone == null) {
 			drone = GameObject.Find ("Drone");
 		} else {
+			// Once we've found it, transform the drone's 3D position to a 2D position corresponding to its position on your 2D screen
 			Vector3 pos = cam.WorldToScreenPoint (drone.transform.position + new Vector3(0, 10));
+
+			// Draw its name there
 			GUI.Label (new Rect (pos.x, Screen.height - pos.y, 100, 100), "Drone");
 		}
 	}
