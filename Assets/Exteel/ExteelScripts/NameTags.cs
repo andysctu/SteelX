47a854fcbf1eeb1db0bc6961dc8bc90faed5c08b
Andy Tu <andy.tu@originate.com>
2016-06-08 17:49:48 -0700

    Drone name working

--- Assets/Exteel/ExteelScripts/NameTags.cs
+++ Assets/Exteel/ExteelScripts/NameTags.cs
@@ -5,23 +5,39 @@ using System.Collections.Generic;
 
 public class NameTags : MonoBehaviour {
 
+	GameManager gm;
+	GameObject drone;
+	Camera cam;
 	// Use this for initialization
 	void Start () 
     {
-        GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+//        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+		drone = GameObject.Find ("Drone");
+		if (drone == null) {
+			Debug.Log ("Drone is null");
+		}
+//		foreach (KeyValuePair<GameObject, Data> entry in gm.playerInfo)
+//		{
+////			aList.Add (GetComponent<GUIText> ().text = entry.Key.name);
+//		}
+		cam = GameObject.Find("Camera").GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        List<GameObject> PlayerNames = gm.playerInfo.Keys.ToList();
-        List<GameObject> aList = new List<GameObject> ();
-	    for (int i = 0; i < PlayerNames.Count; i++)
-        {
-            aList.Add(GetComponent<GUIText>().text = PlayerNames[i].name);
-        }
-        return aList;
-        Debug.Log(PlayerNames);
+//        List<GameObject> aList = new List<GameObject> ();
+
+
+	}
+
+	void OnGUI() {
+		if (drone == null) {
+			drone = GameObject.Find ("Drone");
+		} else {
+			Vector3 pos = cam.WorldToScreenPoint (drone.transform.position + new Vector3(0, 10));
+			GUI.Label (new Rect (pos.x, Screen.height - pos.y, 100, 100), "Drone");
+		}
 	}
  
 }
