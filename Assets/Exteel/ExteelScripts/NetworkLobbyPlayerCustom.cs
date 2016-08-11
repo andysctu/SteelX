072908a55d358ad16e3466d2d1231389c7551bdf
Andy Tu <andy.tu@originate.com>
2016-08-10 18:32:43 -0700

    Custom lobby player GUI

--- Assets/Exteel/ExteelScripts/NetworkLobbyPlayerCustom.cs
+++ Assets/Exteel/ExteelScripts/NetworkLobbyPlayerCustom.cs
@@ -8,4 +8,65 @@ public class NetworkLobbyPlayerCustom : NetworkLobbyPlayer {
 //		name = UserData.myData.User.PilotName;
 //		base.OnClientEnterLobby ();
 //	}
+	void OnGUI()
+	{
+//		if (!ShowLobbyGUI)
+//			return;
+//
+//		var lobby = NetworkManager.singleton as NetworkLobbyManager;
+//		if (lobby)
+//		{
+//			if (!lobby.showLobbyGUI)
+//				return;
+//
+//			string loadedSceneName = SceneManager.GetSceneAt(0).name;
+//			if (loadedSceneName != lobby.lobbyScene)
+//				return;
+//		}
+//
+		Rect rec = new Rect(100 + slot * 100, 200, 90, 20);
+//
+		if (isLocalPlayer)
+		{
+			string youStr;
+			if (readyToBegin)
+			{
+				youStr = "(1Ready)";
+			}
+			else
+			{
+				youStr = "(1Not Ready)";
+			}
+			GUI.Label(rec, youStr);
+
+			if (readyToBegin)
+			{
+				rec.y += 25;
+				if (GUI.Button(rec, "1STOP"))
+				{
+					SendNotReadyToBeginMessage();
+				}
+			}
+			else
+			{
+				rec.y += 25;
+				if (GUI.Button(rec, "1START"))
+				{
+					SendReadyToBeginMessage();
+				}
+
+				rec.y += 25;
+				if (GUI.Button(rec, "1Remove"))
+				{
+					ClientScene.RemovePlayer(GetComponent<NetworkIdentity>().playerControllerId);
+				}
+			}
+		}
+		else
+		{
+			GUI.Label(rec, "1Player [" + netId + "]");
+			rec.y += 25;
+			GUI.Label(rec, "1Ready [" + readyToBegin + "]");
+		}
+	}
 }
