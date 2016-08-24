ce88c4eb729add2845e0a2ff967253ba274f5e8e
Andy Tu <andy.tu@originate.com>
2016-08-23 19:21:22 -0700

    d

--- Assets/Exteel/ExteelScripts/Photon/HangarManager.cs
+++ Assets/Exteel/ExteelScripts/Photon/HangarManager.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HangarManager : MonoBehaviour {
 
+	[SerializeField] GameObject[] Tabs;
+
 	// Use this for initialization
 	void Start () {
-	
+		for (int i = 0; i < Tabs.Length; i++) {
+			Transform tabTransform = Tabs [i].transform;
+			GameObject smallTab = tabTransform.FindChild ("Tab").gameObject;
+			Color c = new Color (255, 255, 255, 63);
+			smallTab.GetComponent<Image> ().color = c;
+			int index = i;
+			smallTab.GetComponent<Button> ().onClick.AddListener (() => activateTab(index));
+			tabTransform.FindChild ("Pane").gameObject.SetActive (i == 0);
+		}
+		Debug.Log (UserData.myData.Mech.Arms);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void activateTab(int index) {
+		for (int i = 0; i < Tabs.Length; i++) {
+			Transform tabTransform = Tabs [i].transform;
+			GameObject smallTab = tabTransform.FindChild ("Tab").gameObject;
+			Color c = new Color (255, 255, 255, 63);
+			smallTab.GetComponent<Image> ().color = c;
+			tabTransform.FindChild ("Pane").gameObject.SetActive (i == index);
+		}
 	}
 }
