76c94fa5eb9f234ff93db5b542191c505fb00fb5
Andy Tu <andysctu@gmail.com>
2017-10-09 09:37:57 -0400

    state

--- Assets/Exteel/ExteelScripts/Photon/MechController.cs
+++ Assets/Exteel/ExteelScripts/Photon/MechController.cs
@@ -68,178 +68,26 @@ public class MechController : Photon.MonoBehaviour {
 		if (CharacterController == null || !CharacterController.enabled){
 			return;
 		}
-
-		// Decrease speed if walking backwards
-		bool isWalkingBackwards = (Input.GetAxis ("Vertical") < 0) && !isHorizBoosting;
-
-		// Transform direction from local space to world space
-		move = transform.TransformDirection(move);
-
-		ySpeed -= Gravity;
-
-		ableToVertBoost = jump && (Input.GetKeyUp(KeyCode.Space) || !Input.GetKey(KeyCode.Space)) && mechCombat.EnoughFuelToBoost();
-
-		if (CharacterController.isGrounded) {
-			grounded = true;
-			jump = false;
-
-			if (Input.GetKey(KeyCode.Space)) {
-				jump = true;
-				grounded = false;
-
-				ySpeed = mechCombat.JumpPower();
-			}
-
-			if (Input.GetKey(KeyCode.LeftShift)) {
-				boost = true;
-
-				mechCombat.DecrementFuel();
-				characterControllerSpeed = mechCombat.BoostSpeed();
-			} else {
-				boost = false;
-
-				mechCombat.IncrementFuel();
-				characterControllerSpeed = mechCombat.MoveSpeed();
-			}
-		} else {
-			Debug.Log("here able: " + ableToVertBoost);
-			if (ableToVertBoost && Input.GetKey(KeyCode.Space)) {
-				boost = true;
-
-				ySpeed = mechCombat.VerticalBoostSpeed();
-				mechCombat.DecrementFuel();
-			} else {
-				mechCombat.IncrementFuel();
-			}
+			
+		if (!CharacterController.isGrounded) {
+			ySpeed -= Gravity;
+			UpdateSpeed(0);
 		}
-
-		updateSpeed(characterControllerSpeed);
-
-		CharacterController.Move(move);
-
-//		// Handle movement in air
-//		if (!CharacterController.isGrounded) {
-//			if (!isVertBoosting) { // If we are in the air and not boosting
-//				ySpeed -= Gravity; // Fall
-//			} else { // If we are in the air and also boosting
-//				ySpeed += mechCombat.VerticalBoostSpeed(); // Start vertical boosting if we've jumped
-//				if (ySpeed > mechCombat.MaxVerticalBoostSpeed()) ySpeed = mechCombat.MaxVerticalBoostSpeed(); // Cap vertical speed
-//			}
-//		} else {
-//			ySpeed = 0;
-//			jumped = false;
-//
-//			// Can't vertical boost while on the ground
-//			ableToVertBoost = false;
-//			isVertBoosting = false;
-//
-//			// Animation
-//			animator.SetBool("Grounded", true);
-//		}
-//			
-//		if (Input.GetButton ("Jump") && !jumped) {
-//			ySpeed = mechCombat.JumpPower();
-//			jumped = true;
-//
-//			// Animation
-//			animator.SetBool("Jump", true);
-//			animator.SetBool("Grounded", false);
-//		} else {
-//			// Animation
-//			animator.SetBool("Jump", false);
-//		}
-
-//		// We are only able to vertically boost if we've jumped, and if we're not already pressing space
-//		if (!ableToVertBoost) {
-//			ableToVertBoost = jumped && (Input.GetKeyUp("space") || !Input.GetKey("space")) && mechCombat.EnoughFuelToBoost();
-//		}
-//
-//		// Prevent starting a boost when below min fuel
-//		if (!isHorizBoosting) {
-//			startBoosting = Input.GetKey ("left shift") && mechCombat.EnoughFuelToBoost();
-//			isHorizBoosting = startBoosting;
-//		} 
-//			
-//		// NOTE: ableToVertBoost and startBoosting depend on EnoughFuelToBoost, whereas if you are already boosting, you just need !FuelEmpty()
-//
-//		Vector3 curPos = camTransform.localPosition;
-//
-//		// Handle horizontal boosting and vertical boosting
-//		if (startBoosting && Input.GetKey ("left shift") && !mechCombat.FuelEmpty()) {
-//			isHorizBoosting = true;
-//			animator.SetBool("Boost", true);
-//
-//			// Camera work
-//			Vector3 newPos = camTransform.localPosition;
-//			float h = Input.GetAxis("Horizontal");
-//			if (h > 0) {
-//				newPos = new Vector3(-7, curPos.y, curPos.z);
-//			} else if (h < 0) {
-//				newPos = new Vector3(7, curPos.y, curPos.z);
-//			} else {
-//				newPos = new Vector3(0, curPos.y, curPos.z);
-//			}
-//			camTransform.localPosition = Vector3.Lerp(camTransform.localPosition, newPos, 0.1f);
-//
-//			// Use fuel while boosting
-//			mechCombat.DecrementFuel();
-//
-//			updateSpeed(mechCombat.BoostSpeed());
-//			photonView.RPC ("Boost", PhotonTargets.All, true);
-//		} else if (ableToVertBoost && (Input.GetKey("space") || Input.GetKey("left shift")) && !mechCombat.FuelEmpty()) {
-//			isVertBoosting = true;
-//			animator.SetBool("Boost", true);
-//
-//			// Camera work
-//			Vector3 newPos = camTransform.localPosition;
-//			float h = Input.GetAxis("Horizontal");
-//			if (h > 0) {
-//				newPos = new Vector3(-7, curPos.y, curPos.z);
-//			} else if (h < 0) {
-//				newPos = new Vector3(7, curPos.y, curPos.z);
-//			} else {
-//				newPos = new Vector3(0, curPos.y, curPos.z);
-//			}
-//			camTransform.localPosition = Vector3.Lerp(camTransform.localPosition, newPos, 0.1f);
-//
-//			// Use fuel while boosting
-//			mechCombat.DecrementFuel();
-//
-//			updateSpeed(mechCombat.BoostSpeed());
-//			photonView.RPC ("Boost", PhotonTargets.All, true);
-//		}
-//		// Handle walking
-//		else {
-//			animator.SetBool("Boost", false);
-//
-//			Vector3 newPos = new Vector3(0, curPos.y, curPos.z);
-//			camTransform.localPosition = Vector3.Lerp(camTransform.localPosition, newPos, 0.1f);
-//			
-//			isHorizBoosting = false;
-//			isVertBoosting = false;
-//
-//			// Recharge fuel while not boosting
-//			mechCombat.IncrementFuel();
-//
-//			updateSpeed(mechCombat.MoveSpeed());
-//			photonView.RPC ("Boost", PhotonTargets.All, false);
-//		}
-
-//		CharacterController.Move (move);
 	}
 
 	void LateUpdate() {
-		animator.SetFloat("Speed", speed);
-		animator.SetFloat("Direction", direction);
-		animator.SetBool("Jump", jump);
-		animator.SetBool("Grounded", grounded);
-		animator.SetBool("Boost", boost);
+//		animator.SetFloat("Speed", speed);
+//		animator.SetFloat("Direction", direction);
+//		animator.SetBool("Jump", jump);
+//		animator.SetBool("Grounded", grounded);
+//		animator.SetBool("Boost", boost);
 	}
 
-	void updateSpeed(float horizontalSpeed) {
+	public void UpdateSpeed(float horizontalSpeed) {
 		move.x *= horizontalSpeed * Time.fixedDeltaTime;
 		move.z *= horizontalSpeed * Time.fixedDeltaTime;
 		move.y += ySpeed * Time.fixedDeltaTime;
+		CharacterController.Move(move);
 	}
 
 	[PunRPC]
---
+++ Assets/GroundedState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedState : StateMachineBehaviour {
+
+	private CharacterController cc;
+	private MechController mctrl;
+	private MechCombat mcbt;
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		cc = animator.transform.parent.gameObject.GetComponent<CharacterController>();
+		mctrl = animator.transform.parent.gameObject.GetComponent<MechController>();
+		mcbt = animator.transform.parent.gameObject.GetComponent<MechCombat>();
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		float speed = Input.GetAxis("Vertical");
+		float direction = Input.GetAxis("Horizontal");
+
+		if (cc.isGrounded) {
+			if (Input.GetKey(KeyCode.Space)) {
+				animator.SetBool("Jump", true);
+
+				mctrl.ySpeed = mcbt.JumpPower();
+				mctrl.UpdateSpeed(0);
+			}
+		} else {
+			
+		}
+
+		animator.SetFloat("Speed", speed);
+		animator.SetFloat("Direction", direction);
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
