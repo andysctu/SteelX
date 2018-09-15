9da005423f0e51146562394dddd2413dffe69b9a
zionch <chengchonz@yahoo.com.tw>
2018-09-15 20:05:30 +0800

    Improve displaying bar string

--- Assets/Exteel/Scripts/Utility/UIExtensionMethods.cs
+++ Assets/Exteel/Scripts/Utility/UIExtensionMethods.cs
@@ -1,26 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Text;
 
-public static class UIExtensionMethods{
-    public static string BarValueToString(int curvalue, int maxvalue) {        
-        int maxvalue_length = (int)Mathf.Log10(maxvalue) + 1;
+public static class UIExtensionMethods {
+    private static StringBuilder builder = new StringBuilder();
 
-        string curvalueStr = (curvalue<0)? "0" : curvalue.ToString();
-        string maxvalueStr = maxvalue.ToString();
-
-        for (int i = 0; i < maxvalue_length - curvalueStr.Length; i++) {
-            curvalueStr.Insert(0, "0");
-        }
-
-        return FillStringWithSpaces(curvalueStr + "/" + maxvalueStr);
+    public static string BarValueToString(int curvalue, int maxvalue) {// 900,2000 -> 0 9 0 0 / 2 0 0 0 , 13000,13000 - > 1 3 0 0 0 / 1 3 0 0 0
+        return FillStringWithSpaces(((curvalue < 0) ? "0" : curvalue.ToString()).PadLeft(4, '0') + "/" + maxvalue.ToString().PadLeft(4, '0'));
     }
 
     public static string FillStringWithSpaces(string str) {
-        string finalStr= "";
-        for(int i = 0; i < str.Length-1; i++) {
-            finalStr += str[i]+ " ";
-        }
-        finalStr += str[str.Length-1];
+        //Clear string builder
+        builder.Length = 0;
+        builder.Capacity = 0;
 
-        return finalStr;
+        for (int i = 0; i < str.Length - 1; i++) {
+            builder.Append(str[i] + " ");
+        }
+        builder.Append(str[str.Length - 1]);
+
+        return builder.ToString();
     }
 }
