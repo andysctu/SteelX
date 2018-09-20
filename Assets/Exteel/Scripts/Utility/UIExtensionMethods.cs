23cab7c0a0d7c005408391398efef193eb62e3bc
zionch <chengchonz@yahoo.com.tw>
2018-09-20 14:00:08 +0800

    improve ui extension method

--- Assets/Exteel/Scripts/Utility/UIExtensionMethods.cs
+++ Assets/Exteel/Scripts/Utility/UIExtensionMethods.cs
@@ -1,13 +1,14 @@
 ﻿using System.Text;
 
 public static class UIExtensionMethods {
+    static StringBuilder builder = new StringBuilder();
 
     public static string BarValueToString(int curValue, int maxvalue) {// 900,2000 -> 0 9 0 0 / 2 0 0 0 , 13000,13000 - > 1 3 0 0 0 / 1 3 0 0 0
         return FillStringWithSpaces(((curValue < 0) ? "0" : curValue.ToString()).PadLeft(4, '0') + "/" + maxvalue.ToString().PadLeft(4, '0'));
     }
 
     public static string FillStringWithSpaces(string str) {
-        StringBuilder builder = new StringBuilder();
+        builder.Length = 0;
 
         for (int i = 0; i < str.Length - 1; i++) {
             builder.Append(str[i]);
