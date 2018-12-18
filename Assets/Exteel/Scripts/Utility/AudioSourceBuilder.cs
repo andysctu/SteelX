69fea332850a1667b625168abb4d03d5606aa111
zionch <chengchonz@yahoo.com.tw>
2018-12-18 19:06:25 +0800

    add utility script

---
+++ Assets/Exteel/Scripts/Utility/AudioSourceBuilder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Utility {
+    class AudioSourceBuilder {
+        public static AudioSource Build(GameObject g, float volume = 1, bool loop = false, float dopplerLevel = 0, float minDistance = 20, float maxDistance = 250, float spatialBlend = 1) {
+            AudioSource audioSource = g.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            audioSource.loop = loop;
+            audioSource.volume = volume;
+            audioSource.dopplerLevel = dopplerLevel;
+            audioSource.maxDistance = maxDistance;
+            audioSource.minDistance = minDistance;
+            audioSource.spatialBlend = spatialBlend;
+
+            return audioSource;
+        }
+    }
+}
